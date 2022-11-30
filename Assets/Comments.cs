46453fd877728b23da34e3bac526e725028ee8e0
Caleb Brazeau <cjbrazeau12@gmail.com>
2022-11-30 02:48:05 -0500

    Add comments to Level1Tutorial.cs

--- Assets/Comments.cs
+++ Assets/Comments.cs
@@ -4,16 +4,23 @@ using UnityEngine;
 
 public class Comments : MonoBehaviour
 { // Good Luck
-    /****** Scripts/Controller.cs: ******/
-    // Line 9: მითითება TextMeshPro UI ელემენტი
-    // Line 11: Variable to store the timer time
-    // Line 15-17: Variables to store seconds minutes and hours
-    // Line 22-25: Reset all varibles to 0 on start
-    // Line 31: Increase time variable by deltaTime every frame
-    // Line 33: Mafs
-    // Line 38: Display timer time
-    // Line 45: Sets the TextMeshPro UI Element to the timer time
-
-    /****** Scripts/Powerup.cs: ******/
-    // Line 8: Variable that stores the different types of powerups
+    /****** Scripts/Level1Tutorial.cs: ******/
+    // Variables to store the prompts shown the the player
+    // Variables to store whether or not the W and D keys have been pressed
+    // Enable the acceleratePrompt for the player to see
+    // Disable the brakePrompt so the prompts are not overlayed
+    // Disable the quitPrompt so the prompts are not overlayed
+    // If the W key has not been pressed and the W key has been pressed
+    // Set the has been pressed variable for W to true
+    // Disable the acceleratePrompt
+    // Enable the brakePrompt to show the user the next action
+    // If the W key has been pressed, the D key has not been pressed, and the S key has been pressed
+    // Set the has been pressed variable for S to true
+    // Disable the brakePrompt 
+    // Enable the quitPrompt to show the user the next action
+    // If both W and S key have been pressed
+    // Start the DestroyPrompt coroutine to eventually remove all prompts
+    // While true
+    // Come back to the line after 5 seconds
+    // Destroy the parent "Prompts" object to remove all prompts
 }
--- Assets/Scripts/Tutorial/Level1Tutorial.cs
+++ Assets/Scripts/Tutorial/Level1Tutorial.cs
@@ -5,40 +5,58 @@ using TMPro;
 
 public class Level1Tutorial : MonoBehaviour
 {
+    // Reference Comments.cs Line 8
     [SerializeField] TextMeshProUGUI acceleratePrompt;
     [SerializeField] TextMeshProUGUI brakePrompt;
     [SerializeField] TextMeshProUGUI quitPrompt;
 
+    // Reference Comments.cs Line 9
     bool boolHasPressedW = false;
-    bool boolHasPressedD = false;
+    bool boolHasPressedS = false;
 
     void Start()
     {
+        // Reference Comments.cs Line 10
         acceleratePrompt.enabled = true;
+        // Reference Comments.cs Line 11
         brakePrompt.enabled = false;
+        // Reference Comments.cs Line 12
         quitPrompt.enabled = false;
     }
 
     void Update()
     {
+        // Reference Comments.cs Line 13
         if (!boolHasPressedW && Input.GetKeyDown(KeyCode.W)) {
+            // Reference Comments.cs Line 14
             boolHasPressedW = true;
+            // Reference Comments.cs Line 15
             acceleratePrompt.enabled = false;
+            // Reference Comments.cs Line 16
             brakePrompt.enabled = true;
-        } else if (boolHasPressedW && !boolHasPressedD && Input.GetKeyDown(KeyCode.S)) {
-            boolHasPressedD = true;
+        // Reference Comments.cs Line 17
+        } else if (boolHasPressedW && !boolHasPressedS && Input.GetKeyDown(KeyCode.S)) {
+            // Reference Comments.cs Line 18
+            boolHasPressedS = true;
+            // Reference Comments.cs Line 19
             brakePrompt.enabled = false;
-        } else if (boolHasPressedW && boolHasPressedD) {
+            // Reference Comments.cs Line 20
             quitPrompt.enabled = true;
+        // Reference Comments.cs Line 21
+        } else if (boolHasPressedW && boolHasPressedS) {
+            // Reference Comments.cs Line 22
             StartCoroutine(DestroyPrompts());
         }
     }
 
     IEnumerator DestroyPrompts()
     {
+        // Reference Comments.cs Line 23
         while(true)
         {
+            // Reference Comments.cs Line 24
             yield return new WaitForSeconds(5f);
+            // Reference Comments.cs Line 25
             Destroy(brakePrompt.transform.parent.gameObject);
         }
     }
